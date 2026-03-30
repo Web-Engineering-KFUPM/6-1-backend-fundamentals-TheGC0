d0b3a592fb06ac2e932475e8c41dde139564757c
AliShamah <ali_shamah@outlook.com>
2026-03-30 11:55:39 +0300

    add cors middleware and update package dependencies

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,13 +1,16 @@
 // TODO 1: Import Express
 import express from 'express';
+import cors from 'cors';
 
 
 
 // TODO 2: Create the Express app and store it in a variable named app
+const app = express();
 
 
 
 // TODO 3: Allow React to access the server
+app.use(cors());
 
 
 
